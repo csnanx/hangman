1bfe83d632bc8d34997c2f82e595901006483d0d
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-21 08:46:38 +0530

    Improve UX

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
+from hangman_art import stages
+from hangman_words import word_list
 
-word_list = ["aardvark", "baboon", "camel"]
 lives = 6
 random_word = random.choice(word_list)
 print(random_word)
@@ -10,21 +11,28 @@ for _ in random_word:
     dashes += "_"
 print(dashes)
 
+correct_letters = []
 guessed_letters = []
 game_over = False
 
 while not game_over:
+    print(f"**********{lives}/6 LIVES LEFT**********")
     guess = input("Guess a letter: ").lower()
     display = ""
 
-    if guess not in random_word:
+    if guess in guessed_letters:
+        print(f"You've already guessed {guess}")
+    elif guess not in random_word:
         lives -= 1
+        print(f"Your guess {guess} is not in the word.\nYou lose a life.")
+
+    guessed_letters.append(guess)
 
     for char in random_word:
         if char == guess:
             display += guess
-            guessed_letters.append(guess)
-        elif char in guessed_letters:
+            correct_letters.append(guess)
+        elif char in correct_letters:
             display += char
         else:
             display += "_"
@@ -33,9 +41,9 @@ while not game_over:
 
     if "_" not in display:
         game_over = True
-        print("You win")
+        print("**********You WIN**********")
     elif lives == 0:
         game_over = True
-        print("You lose")
+        print("**********You LOSE**********")
 
     print(stages[lives])
